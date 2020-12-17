f72d8ca2c7d10f886c9a15864e89d3c636783a48
Rodrigo Mariano <rodmariano13@gmail.com>
2020-12-17 12:32:41 -0300

    feat: validate null and empty emails

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,16 @@
+import { Email } from './email'
+
+describe('Email validation', () => {
+    test('should not accept null strings', () => {
+        const email = null
+        expect(Email.validate(email)).toBeFalsy()
+    })
+    test('should not accept empty strings', () => {
+        const email: string = ''
+        expect(Email.validate(email)).toBeFalsy()
+    })
+    test('should not accept valid email', () => {
+        const email: string = 'any@mail.com'
+        expect(Email.validate(email)).toBeTruthy()
+    })
+})
