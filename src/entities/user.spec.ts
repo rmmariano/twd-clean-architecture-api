d7752eb3f4c1da7042fcc9938f40fd5b0a264034
Rodrigo Mariano <rodmariano13@gmail.com>
2020-12-21 15:21:29 -0300

    test: create valid user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -4,6 +4,14 @@ import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('User domain entity', () => {
+    test('should create user with valid data', () => {
+        const validName = 'any_name'
+        const validEmail = 'any@mail.com'
+        const user: User = User.create({ name: validName, email: validEmail }).value as User
+        expect(user.name.value).toEqual(validName)
+        expect(user.email.value).toEqual(validEmail)
+    })
+
     test('should not create user with invalid name (too few characteres)', () => {
         const invalidName = 'O          '
         const error = User.create({ name: invalidName, email: 'any@mail.com' })
