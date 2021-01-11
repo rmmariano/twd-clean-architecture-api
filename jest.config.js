7f01d1842b5f6e8b9175dec746200727f437a8ad
Rodrigo Mariano <rodmariano13@gmail.com>
2021-01-11 10:18:26 -0300

    refactor: fix indentation

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/src'],
+    roots: ['<rootDir>/src'],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
