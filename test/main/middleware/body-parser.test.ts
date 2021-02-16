0985c733d21055c85c45a93101e4def323a785e6
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-16 11:08:13 -0300

    fix: fix app.ts file name

--- test/main/middleware/body-parser.test.ts
+++ test/main/middleware/body-parser.test.ts
@@ -1,15 +1,14 @@
-// import request from 'supertest'
-// import app from '@/main/config/app'
+import request from 'supertest'
+import app from '@/main/config/app'
 
 describe('Body parser middleware', () => {
     test('should parse body as JSON', async () => {
-        // app.post('/test_body_parser', (req, res) => {
-        //     res.send(req.body)
-        // })
-        // await request(app)
-        //     .post('/test_body_parser')
-        //     .send({ name: 'Otavio' })
-        //     .expect({ name: 'Otavio' })
-        expect(0).toBe(0)
+        app.post('/test_body_parser', (req, res) => {
+            res.send(req.body)
+        })
+        await request(app)
+            .post('/test_body_parser')
+            .send({ name: 'Otavio' })
+            .expect({ name: 'Otavio' })
     })
 })
