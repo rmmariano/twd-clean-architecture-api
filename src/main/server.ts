494068d22e07ddc1f078f5cf60aad8b1c8971ab6
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-27 13:22:35 -0300

    chore: add docker compose with mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://root:root@localhost:27017')
     .then(async () => {
         const app = (await import('./config/app')).default
         app.listen(5000, () => {
