446769f496e1a66f5de0d967f3ecbbf2ed64a6b8
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-27 11:23:17 -0300

    test: add user and verify existence using mongodb

---
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
+import { MongodbUserRepository } from '@/external/repositories/mongodb'
+
+describe('Mongodb User repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+    beforeEach(async () => {
+        MongoHelper.clearCollection('users')
+    })
+    test('when user is added, it should exist', async () => {
+        const userRepository = new MongodbUserRepository()
+        const user = {
+            name: 'Any Name',
+            email: 'any@mail.com'
+        }
+        await userRepository.add(user)
+        expect(await userRepository.exists(user)).toBeTruthy()
+    })
+})
